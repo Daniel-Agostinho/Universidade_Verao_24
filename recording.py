e9e4d2f8573ed5d2ba87bb73a66d985948f1faf4
Daniel <danielagostinho1@gmail.com>
2024-07-15 16:42:57 +0100

    First commit

---
+++ recording.py
@@ -0,0 +1,32 @@
+from src.record_tools import Device
+import time
+import keyboard
+
+
+def main():
+
+    # Bitalino configuration
+    bitalino_address = "00:21:08:35:15:17"
+    bitalino_sampling_rate = 1000
+    bitalino_channels = [0, 1, 2]
+
+    session_id = int(input("Session ID: "))
+    device = Device(bitalino_address, bitalino_sampling_rate, bitalino_channels, session_id)
+
+    # Starting session acquisition
+    print("Start session acquisition")
+    device.start()
+
+    while True:
+        key = keyboard.read_key()
+        device.state = key
+
+        if key == "q":
+            device.stop()
+            break
+
+    print("Ending session")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/record_tools.py
@@ -0,0 +1,105 @@
+import csv
+import os
+import time
+import threading
+import numpy as np
+from bitalino import BITalino
+
+
+class Device:
+    def __init__(self, address, sampling_rate, channels, session_id):
+        self.device = connect_bitalino(address)
+        self.sampling_rate = sampling_rate
+        self.channels = channels
+        self.session_id = session_id
+        self.save_file = self.open_file()
+        self.state = 0
+        self.sample_number = 0
+        self.session = False
+        self.session_thread = None
+
+    def start(self):
+        self.session_thread = threading.Thread(target=self.start_acquisition)
+        self.session_thread.start()
+
+    def start_acquisition(self):
+        self.session = True
+        self.device.start(self.sampling_rate, self.channels)
+
+        while self.session:
+            data = self.get_data()
+            self.save_data(data)
+
+    def get_data(self):
+        time_stamp = np.linspace(self.sample_number, self.sample_number + 1 - 1/self.sampling_rate, self.sampling_rate)
+        states = np.zeros(self.sampling_rate).tolist()
+
+        raw_data = self.device.read(self.sampling_rate)
+
+        if self.state != 0:
+            states[0] = self.state
+            self.state = 0
+
+        sensor1 = raw_data[:, -3]
+        sensor2 = raw_data[:, -2]
+        sensor3 = raw_data[:, -1]
+
+        data = np.vstack((time_stamp, sensor1, sensor2, sensor3, states)).transpose()
+
+        self.sample_number += 1
+
+        return data
+
+    def save_data(self, data):
+        with open(self.save_file, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+    def stop(self):
+        self.session = False
+        self.session_thread.join()
+        self.device.stop()
+        self.device.close()
+
+    def open_file(self):
+        file_fpath = os.path.join(
+            "Data",
+        )
+
+        if not os.path.isdir(file_fpath):
+            os.makedirs(file_fpath)
+
+        file_name = os.path.join(
+            file_fpath,
+            f"session_{self.session_id}.csv",
+        )
+
+        with open(file_name, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Time", "Sensor1", "Sensor2", "Sensor3", "Trigger"])
+
+        return file_name
+
+
+def connect_bitalino(address):
+    connected = False
+    device = None
+
+    while not connected:
+        try:
+            print(f'Will connect to BITalino {address}')
+            device = BITalino(address, timeout=5)
+            connected = True
+
+        except Exception:
+            print("Failed to connect to device")
+            print("Trying again in 2 seconds")
+            time.sleep(2)
+
+    print(f"Connected to device: {address}")
+
+    return device
+
+
+if __name__ == '__main__':
+    pass
